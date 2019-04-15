a66504aa148d414d79d0d1e012468f5b693906e0
Jonas Merhej <jonasmerhej1@gmail.com>
2019-04-15 08:52:28 +0200

    release v1.0

---
+++ README.md
@@ -0,0 +1,18 @@
+# Gutenberg Slider
+
+A WordPress Gutenberg Slider
+
+## Features
+
+* Slider block wrapper that accepts only slide blocks
+* Slide block that saves InnerBlocks
+
+## Setup
+
+Run yarn or npm install
+
+## Changelog
+
+### v0.1.0
+
+* Initial release
---
+++ src/js/slide/renderer.js
@@ -0,0 +1,77 @@
+/**
+* Wordpress dependencies
+*/
+const { InnerBlocks } = wp.editor
+const { Fragment } = wp.element;
+
+
+
+const Renderer = ( { attributes } ) => {
+    /**
+    * Slide inline style
+    */
+    const styles= () => {
+            if ( attributes.backgroundImage && attributes.backgroundColor ) {
+                return {
+                    backgroundImage: 'url(' + attributes.backgroundImage + ')',
+                    backgroundColor: attributes.backgroundColor,
+                }
+            }
+            else if ( attributes.backgroundImage ) {
+                return {
+                    backgroundImage: 'url(' + attributes.backgroundImage + ')'
+                }
+            }
+            else if ( attributes.backgroundColor ) {
+                return {
+                    backgroundColor: attributes.backgroundColor
+                }
+            }
+            else ''
+        }
+
+
+  // UI
+  return (
+    <Fragment>
+        <div
+          className='jonasm-slide slide'
+          style={ styles() }
+        >
+          { attributes.image_D || attributes.image_L || attributes.image_P || attributes.image_M ?
+                <div className='slide-media__wrapper'>
+                    <picture className='slide-media'>
+                    { attributes.image_D ?
+                        <source media='(min-width: 1439px)' srcSet={ attributes.image_D } />
+                    :
+                        ''
+                    }
+                    { attributes.image_L ?
+                        <source media='(min-width: 1023px)' srcSet={ attributes.image_L } />
+                    :
+                        ''
+                    }
+                    { attributes.image_P ?
+                        <source media='(min-width: 767px)' srcSet={ attributes.image_P } />
+                    :
+                        ''
+                    }
+                        <img
+                            className='slide-media__image'
+                            src={ attributes.image_M }
+                        />
+                    </picture>
+                </div>
+                : ''
+            }
+            <div className='jonasm-slide__content-wrapper slide__content-wrapper'>
+                <div className='jonasm-slide__content slide__content'>
+                    <InnerBlocks.Content />
+                </div>
+            </div>
+        </div>
+    </Fragment>
+  )
+}
+
+export default Renderer

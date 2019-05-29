e86fcd46d850d25256fe4e56dd394cc35bd19ba3
Jonas Merhej <jonasmerhej1@gmail.com>
2019-05-29 19:25:25 +0000

    Apply fixes from StyleCI

--- plugin.php
+++ plugin.php
@@ -5,9 +5,7 @@
  * Author:          Jonas Merhej
  * Text Domain:     gutenberg-slider
  * Domain Path:     /languages
- * Version:         0.1.0
- *
- * @package         jonasm/gutenberg-slider
+ * Version:         0.1.0.
  */
 
 namespace jonasm\GutenbergSLider;
@@ -15,20 +13,18 @@ namespace jonasm\GutenbergSLider;
 //  Exit if accessed directly.
 defined('ABSPATH') || exit;
 
-
-/**
+/*
  * Enqueue block script and backend stylesheet.
  */
-add_action( 'enqueue_block_editor_assets', function() {
-	wp_enqueue_script( 'gutenberg-slider', plugins_url( '/dist/js/block.js', __FILE__ ), [], filemtime( plugin_dir_path( __FILE__ ) . 'dist/js/block.js' ), TRUE );
-	wp_enqueue_script( 'plain-js', plugins_url( '/src/ui-js/index.js', __FILE__ ), [], filemtime( plugin_dir_path( __FILE__ ) . 'src/ui-js/index.js'), TRUE);
-	wp_enqueue_style( 'gutenberg-slider-editor', plugins_url( 'dist/css/editor.css', __FILE__ ) );
-} );
+add_action('enqueue_block_editor_assets', function () {
+    wp_enqueue_script('gutenberg-slider', plugins_url('/dist/js/block.js', __FILE__), [], filemtime(plugin_dir_path(__FILE__).'dist/js/block.js'), true);
+    wp_enqueue_script('plain-js', plugins_url('/src/ui-js/index.js', __FILE__), [], filemtime(plugin_dir_path(__FILE__).'src/ui-js/index.js'), true);
+    wp_enqueue_style('gutenberg-slider-editor', plugins_url('dist/css/editor.css', __FILE__));
+});
 
-
-/**
+/*
  * Load translation
  */
-add_action( 'plugins_loaded', function(){
-	load_plugin_textdomain( 'gutenberg-slider', false, plugin_dir_path( __FILE__ ) . 'languages' );
+add_action('plugins_loaded', function () {
+    load_plugin_textdomain('gutenberg-slider', false, plugin_dir_path(__FILE__).'languages');
 });
